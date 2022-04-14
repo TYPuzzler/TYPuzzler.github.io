59f6d2a0a651992ccb4ac2a4afb631f76e8c6f9c
Bowen Tian <bowent2@cs.uw.edu>
2022-04-13 19:45:35 -0700

    create puzzleCreator.py

---
+++ src/puzzleCreator.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class PuzzlePiece:
+    def __init__(self, coords, imgSource):
+        self.coords = coords
+        self.source = imgSource
+
+    def cutImg(img, coords):
+
+
+    def getPiece():
+        return self.
