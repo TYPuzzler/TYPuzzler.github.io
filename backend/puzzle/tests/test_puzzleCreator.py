c4a2c528ca0030d9727827aa5596e01dd644653b
Bowen Tian <c.tian1997@gmail.com>
2022-05-03 19:49:57 -0700

    basic test added

---
+++ backend/puzzle/tests/test_puzzleCreator.py
@@ -0,0 +1,7 @@
+import pytest
+from src.puzzleCreator import *
+
+# test number of puzzle
+def test_number_of_pieces():
+	p = Puzzle('../../images/Python_(programming_language).png', 30, 'test')
+	assert len(p.pieces) == 1135
