ded42752a814bd29254ef11b06761723472186e6
Bowen Tian <c.tian1997@gmail.com>
2022-05-03 21:25:36 -0700

    python style checked

--- backend/puzzle/tests/test_puzzleCreator.py
+++ backend/puzzle/tests/test_puzzleCreator.py
@@ -1,7 +1,8 @@
 import pytest
 from src.puzzleCreator import *
 
+
 # test number of puzzle
 def test_number_of_pieces():
-	p = Puzzle('images/Python_(programming_language).png', 30, 'test')
-	assert len(p.pieces) == 1135
+    p = Puzzle('images/Python_(programming_language).png', 30, 'test')
+    assert len(p.pieces) == 1135
