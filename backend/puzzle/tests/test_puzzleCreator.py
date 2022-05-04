f8b92b84b63d3b00bb5b19f81302de95a8ee8cf3
Bowen Tian <c.tian1997@gmail.com>
2022-05-03 19:55:45 -0700

    path fixed

--- backend/puzzle/tests/test_puzzleCreator.py
+++ backend/puzzle/tests/test_puzzleCreator.py
@@ -3,5 +3,5 @@ from src.puzzleCreator import *
 
 # test number of puzzle
 def test_number_of_pieces():
-	p = Puzzle('../../images/Python_(programming_language).png', 30, 'test')
+	p = Puzzle('images/Python_(programming_language).png', 30, 'test')
 	assert len(p.pieces) == 1135
