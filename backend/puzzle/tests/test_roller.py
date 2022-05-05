668cded51492b547bac7669d5d77e52109bc31da
Bowen Tian <c.tian1997@gmail.com>
2022-05-05 01:26:11 -0700

    roller test added

---
+++ backend/puzzle/tests/test_roller.py
@@ -0,0 +1,13 @@
+import pytest
+from src.roller import *
+
+
+# test roller function
+def test_if_get_all_pieces():
+    name = 'JS_logo'
+    ls = []
+    for i in range(100):
+        url = roll(name)
+        ls.append(url)
+        print(url)
+    assert len(url) == 25
