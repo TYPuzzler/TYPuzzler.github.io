8713ce1820cd0e61449513e64d3f788f56467e37
Bowen Tian <c.tian1997@gmail.com>
2022-05-05 01:46:52 -0700

    test

--- backend/puzzle/src/roller.py
+++ backend/puzzle/src/roller.py
@@ -4,10 +4,10 @@ import random
 # or a message that says "No piece left". Uses a text log to keep track of which
 # pieces are obtained.
 def roll(nameOfPuzzle):
-    meta = open('../../images/' + nameOfPuzzle + '/metadata.txt', 'r')
+    meta = open('images/' + nameOfPuzzle + '/metadata.txt', 'r')
     ml = meta.readlines()
     meta.close()
-    with open('../../images/' + nameOfPuzzle + '/obtained.txt', 'r') as obt:
+    with open('images/' + nameOfPuzzle + '/obtained.txt', 'r') as obt:
         ols = []
         ols = obt.readlines()
     if len(ols) == int(ml[2]):
@@ -17,7 +17,7 @@ def roll(nameOfPuzzle):
     n = random.randint(1, int(ml[2]))
     while n in ol:
         n = random.randint(1, int(ml[2]))
-    with open('../../images/' + nameOfPuzzle + '/obtained.txt', 'a') as obt:
+    with open('images/' + nameOfPuzzle + '/obtained.txt', 'a') as obt:
         obt.write(str(n)+'\n')
     url = 'https://github.com/TYPuzzler/TYPuzzler.github.io/blob/main/images/'\
         + nameOfPuzzle + '/' + nameOfPuzzle + '_piece_' + str(n)\
