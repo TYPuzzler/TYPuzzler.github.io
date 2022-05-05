4c215b04078eb65d37ab39d8d6e8a8c150b593f2
Bowen Tian <c.tian1997@gmail.com>
2022-05-05 01:21:29 -0700

    test

--- backend/puzzle/tests/test_puzzle.py
+++ backend/puzzle/tests/test_puzzle.py
@@ -4,6 +4,6 @@ from src.puzzle import *
 
 # test number of puzzle
 def test_number_of_pieces():
-    url = 'https://github.com/TYPuzzler/TYPuzzler.github.io/blob/main/images/Python.png?raw=true'
+    url = 'https://github.com/TYPuzzler/TYPuzzler.github.io/blob/main/images/python.png?raw=true'
     p = Puzzle(url, 30, 'test')
     assert len(p.pieces) == 1135
