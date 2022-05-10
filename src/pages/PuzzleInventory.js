7cd0def29166c833443ece85bd9a2593e2b9633c
Margaret <marg33@cs.washington.edu>
2022-05-10 13:09:04 -0700

    UI changes and deployed again

--- src/pages/PuzzleInventory.js
+++ src/pages/PuzzleInventory.js
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 
 function PuzzleInventory() {
   var puzzle = "https://github.com/TYPuzzler/TYPuzzler.github.io/blob/main/images/JS_logo/full_puzzle_JS_logo.png?raw=true"
+  
   return (
     <div className="App">
-      PuzzleInventory
+      <p>Here's your puzzle progress so far. Keep it up!</p>
+      <br/>
+      <img src={puzzle}></img>
+      <br/>
       <Link to="/">
         <button variant="outlined">
           Home
         </button>
       </Link>
-      <br/>
-      <img src={puzzle}></img>
     </div>
   );
 }
