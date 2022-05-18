3f4565c7b0e2420c476d296eab58a1c3a37aa16c
Bowen Tian <c.tian1997@gmail.com>
2022-05-17 17:27:15 -0700

    rarity files added for husky logo and python logo

--- src/puzzle/util.py
+++ src/puzzle/util.py
@@ -1,3 +1,4 @@
+import urllib.request
 # This script contains all the utilily classes and functions
 
 # Base case exception.
@@ -10,3 +11,35 @@ class PuzzlePieceException(PuzzleException):
 # Raised when user ask for a non-existing piece.
 class NoSuchPieceException(PuzzlePieceException):
 	pass
+
+def writeRarity(nameOfPuzzle):
+	meta = open('../../images/' + nameOfPuzzle + '/metadata.txt', 'r')
+	ml = meta.readlines()
+	nL = []
+	rL = []
+	srL = []
+	ssrL = []
+	for i in range(4, len(ml), 4):
+		num = ml[i].splitlines()[0]
+		rarity = str(ml[i + 2]).splitlines()[0]
+		if rarity == 'N':
+			nL.append(num)
+		elif rarity == 'R':
+			rL.append(num)
+		elif rarity == 'SR':
+			srL.append(num)
+		elif rarity == 'SSR':
+			ssrL.append(num)
+
+	f = open('../../images/' + nameOfPuzzle + '/N.txt', 'w')
+	f.writelines("\n".join(nL))
+	f.close
+	f = open('../../images/' + nameOfPuzzle + '/R.txt', 'w')
+	f.writelines("\n".join(rL))
+	f.close
+	f = open('../../images/' + nameOfPuzzle + '/SR.txt', 'w')
+	f.writelines("\n".join(srL))
+	f.close
+	f = open('../../images/' + nameOfPuzzle + '/SSR.txt', 'w')
+	f.writelines("\n".join(ssrL))
+	f.close
