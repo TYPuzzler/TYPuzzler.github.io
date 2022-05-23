1d94bb6f1d2e8c4fdff8745a7c0cad89845b10f6
Bowen Tian <c.tian1997@gmail.com>
2022-05-23 16:10:29 -0700

    test and tests folders merged

--- test/test_puzzle.py
+++ test/test_puzzle.py
@@ -8,4 +8,4 @@ from puzzle import *
 def test_number_of_pieces():
     url = 'https://github.com/TYPuzzler/TYPuzzler.github.io/blob/main/images/python.png?raw=true'
     p = Puzzle(url, 30, 'test')
-    assert len(p.pieces) == 1135`
+    assert len(p.pieces) == 1135
