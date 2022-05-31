e4b69c851b697a8ee1e182877ac14e7ea6a462ba
Chris Choi <sjchoi1@cs.washington.edu>
2022-05-31 15:31:40 -0700

    Implemented random puzzle piece reward
    Created a way for the user to start over.
    Reward users with random puzzle pieces
    Made the most recently rewarded puzzle piece appear different from
    previously earned ones.

--- src/pages/Typing.js
+++ src/pages/Typing.js
@@ -3,13 +3,32 @@ import useKeyPress from '../hooks/useKeypress';
 import { Link } from "react-router-dom";
 import { generate } from '../utils/words'
 import React, { useCallback, useState } from 'react';
-import { draw } from '../utils/drawPuzzle';
+import { randomPiece } from '../utils/randomPiece';
 
 /**
  * This represents the page that is presented to the user when they start a typing session.
  */
 function Typing() {
-  var level = parseInt(localStorage.getItem("level")) || 0;
+  var level = parseInt(localStorage.getItem("level")) || 1;
+  var puzzleName;
+  if (level == 1) {
+    puzzleName = "JS_logo";
+  } else if (level == 2) {
+    puzzleName = "husky_logo";
+  } else {
+    puzzleName = "python_logo";
+  }
+
+  const pieceCounts = {
+    1 : 25,
+    2 : 108,
+    3 : 108
+}
+
+
+  var earnedPieces = JSON.parse(localStorage.getItem(puzzleName)) ?? Array(109).fill(0);
+
+  const [reward, setReward] = useState([]);
 
   // The substring of the sample text that has been correctly typed
   const [correctChars, setCorrectChars] = useState('');
@@ -34,14 +53,18 @@ function Typing() {
   // The characters that the user has typed
   const[typed, setTyped] = useState('');
 
+  const[nextButton, setNextButton] = useState([]);
+
 
   const fetchData = useCallback(async () => {
     // Get text sample from generator
-    const data = await generate()
+    const text = await generate(50);
+    const rand = await randomPiece(puzzleName);
+    setReward(rand);
 
     // set state with the result
-    setCurrentChar(data.charAt(0));
-    setIncomingChars(data.substring(1));
+    setCurrentChar(text.charAt(0));
+    setIncomingChars(text.substring(1));
   });
 
   React.useEffect(() => {
@@ -69,11 +92,19 @@ function Typing() {
         setIncomingChars(incomingChars.substring(1));
         if (incomingChars.length == 0) {
           // User is done typing, so let's calculate the user's WPM and Accuracy
-
           var duration = ((new Date().getTime()) - startTime) / 60000.0; // convert to minutes
           setWpm("WPM: " + ((correctChars.length / 5) / duration).toFixed(2)); 
           setAccuracy("ACC: " + ((correctChars.length * 100) / typed.length).toFixed(2) + "%")
-          localStorage.setItem("level", (level + 1));
+
+          
+          earnedPieces[0] = earnedPieces[0] + 1;
+          earnedPieces[reward[0]] = 1;
+          localStorage.setItem("level", JSON.stringify(level));
+          localStorage.setItem("last_earned", JSON.stringify(reward));
+          localStorage.setItem(puzzleName, JSON.stringify(earnedPieces));
+          setNextButton([<button variant="outlined">
+          Next
+        </button>]);
         }
       }
     });
@@ -101,9 +132,7 @@ function Typing() {
       </p>
       <KeyPress></KeyPress>
       <Link to="/puzzleSelect">
-        <button variant="outlined">
-          Next
-        </button>
+        {nextButton}
       </Link>
     </div>
   );
--- src/utils/drawPuzzle.js
+++ src/utils/drawPuzzle.js
@@ -1,27 +1,40 @@
+
+
 /**
- * Returns the html for the puzzle reward given the user's level
+ * Returns the html for the user's puzzle progress
+ * @param {*} level The user's level
+ * @param {*} earnedPieces The pieces that the user has earned
+ * @param {*} last_earned The last piece that the user has earned
+ * @returns 
  */
-export const draw = async (level = 1) => {
-    var pieceCount = level;
-    var result = [];
-    var puzzleName;
-    var gray;
-    var pieces;
-    if (level <= 25) {
-        puzzleName = "JS_logo";
-    } else if (level <= (25 + 108)) {
-        puzzleName = "husky_logo"
-        pieceCount = pieceCount - 25;
-    } else {
-        puzzleName = "python_logo"
-        pieceCount = pieceCount - 25 - 108;
+export const draw = async (level = 1, earnedPieces = Array(109).fill(0), last_earned) => {
+    const puzzles = {
+        1 : "JS_logo",
+        2 : "husky_logo",
+        3 : "python_logo"
     }
 
-    // Get the grayscale background
+    const pieceCounts = {
+        1 : 25,
+        2 : 108,
+        3 : 108
+    }
+
+    var result = [];
+
+    // Get the name of the puzzle that the user is currently working on
+    var puzzleName;
+    if (level < 1 || level > 3) {
+        puzzleName = puzzles[1];
+    } else {
+        puzzleName = puzzles[level];
+    }
+    var gray;
+
+    // Get the puzzle outline (previously a grayscale background)
     gray = await getGray(puzzleName);
     var inventory = {
         position: 'relative',
-        // TODO: Replace with puzzle dimensions
         width: gray[1], 
         height: gray[2],
         marginLeft: 'auto',
@@ -29,16 +42,30 @@ export const draw = async (level = 1) => {
         "borderStyle": 'solid',
         "border-width": '3px',
         "border-color": '#000000'
-      };
-    result.push(<img style={{position : 'absolute', top :'0px', left: '0px'}}></img>);
+    };
 
     // Add the pieces earned on top of the gray scale background
-    if (level > 0) {
+    var pieces;
+    if (earnedPieces[0] >= pieceCounts[level]) {
+        pieces = await getFull(puzzleName);
+        result.push(<img src={pieces[0]} style={{position : 'absolute',
+                    left : '0px',
+                    top : '0px'}}></img>)
+    } else if (earnedPieces[0] > 0) {
+        // User has earned at least one puzzle piece
         pieces = await metaData(puzzleName);
-        for (var i = 1; i <= pieceCount; i++) {
-            result.push(<img src={pieces[i][0]} style={{position : 'absolute',
-                left : pieces[i][2] + "px",
-                top : pieces[i][3] + "px"}}></img>);
+        for (var i = 1; i <= pieceCounts[level]; i++) {
+            if (last_earned[0] == i) { {
+                // TODO: draw last earned piece normally
+                result.push(<img src={last_earned[1]} style={{position : 'absolute',
+                    left : last_earned[3] + "px",
+                    top : last_earned[4] + "px"}}></img>);
+            }
+            } else if (earnedPieces[i] == 1) {
+                result.push(<img src={pieces[i][0]} style={{position : 'absolute',
+                    left : pieces[i][1] + "px",
+                    top : pieces[i][2] + "px"}}></img>);
+            }
         }
     }
     
@@ -56,14 +83,13 @@ export const draw = async (level = 1) => {
  * @returns JSON metadata of the puzzle pieces
  */
 export const metaData = async (puzzleName = "JS_logo") => {
-    var url = "https://typuzzler.pythonanywhere.com/meta/\"" + puzzleName + "\"";
+    var url = "https://typuzzler.pythonanywhere.com/trans/\"" + puzzleName + "\"";
     var result;
     await fetch(url)
         .then(response => response.json())
         .then(data => {
             result = data;
         });
-    console.log(result)
     return result;
 }
 
@@ -80,6 +106,16 @@ export const getGray = async (puzzleName = "JS_logo") => {
         .then(data => {
             result = data;
         })
-    console.log(result)
+    return result;
+}
+
+export const getFull = async (puzzleName = "JS_logo") => {
+    var url = "https://typuzzler.pythonanywhere.com/full/\"" + puzzleName + "\"";
+    var result;
+    await fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            result = data;
+        })
     return result;
 }
